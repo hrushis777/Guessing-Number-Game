74e9dd5cd7f2ac2f7d33d47fa086c9427f53f65a
Hrushi <hrushis@prdxn.com>
2021-06-13 18:11:40 +0530

    Final Commit

--- js/script.js
+++ js/script.js
@@ -45,12 +45,13 @@ document.querySelector('.check').addEventListener('click', function () {
     //   }
     // }
 
-    //After Using DRY Principle/ Removing Duplicacy. 
+    //After Using DRY Principle/ Removing Duplicacy.
   } else if (guess !== secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
         guess > secretNumber ? 'Too high!' : 'Too Low!';
       score--;
+      document.querySelector('body').style.backgroundColor = 'red';
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'You lost the Game!';
